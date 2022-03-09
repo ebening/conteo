b29ad03dad4565ea29c9ff7d0b6b1cda6c4bd326
Edgar <ebening@gmail.com>
2022-03-09 00:42:23 -0600

    adding fragments

--- app/src/main/java/com/iasd/conteo/MainActivity.kt
+++ app/src/main/java/com/iasd/conteo/MainActivity.kt
@@ -2,10 +2,40 @@ package com.iasd.conteo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+   /* HomeFragment homeFragment = new HomeFragment();
+    AssistantsFragment assistantsFragment = new = AssistantsFragment();
+    SettingsFragment settingsFragment = SettingsFragment();
+*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       /*val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+       val navController = findNavController(R.id.fragmentContainerView2)*/
+
+       val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+       val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+       bottomNavigationView.setupWithNavController(navHostFragment.navController)
+
+
+       val navController = navHostFragment.navController
+       val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.assistantsFragment, R.id.settingsFragment))
+       setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+
+
+
     }
+
+
 }
